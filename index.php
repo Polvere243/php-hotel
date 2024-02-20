8ce04019a9688c5147b40b417f413dac52921442
Polvere243 <danielercoli@gmail.com>
2024-02-21 00:48:14 +0100

    Created table, still to fix

--- index.php
+++ index.php
@@ -13,10 +13,35 @@
 <body>
     <ul>
         <?php foreach ($hotels as $hotel) : ?>
-            <?php foreach ($hotel as $building) :?>
-                <li>
-                <?= $building ?> 
-                </li>
+            <?php foreach ($hotel as $key => $building) :?>
+               <table>
+                <tr>
+                    <th>
+                        <?= $key ?>
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <?= $building['name'] ?>
+                    </td>
+                    <td>
+                        <?= $building['description'] ?>
+                    </td>
+                    <td>
+                        <?= $building['parking'] ?>
+                    </td>
+                    <td>
+                        <?= $building['vote'] ?>
+                    </td>
+                    <td>
+                        <?= $building['distance_to_center'] ?>
+                    </td>
+                </tr>
+                
+
+               </table>  
+                
+                
             <?php endforeach ?>
         <?php endforeach ?>
     </ul>
